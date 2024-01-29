8b6618bd31197e2f567b8882de760b82e73aa424
Mr. Fast <33694976+HerrSchnell@users.noreply.github.com>
2024-01-29 09:34:46 -0800

    Initial commit

---
+++ reverse-arrays.js
@@ -0,0 +1,20 @@
+function reverseArray(arr) {
+  // Add code.
+}
+
+function reverseArrayInPlace(arr) {
+  // Add code.
+}
+
+
+
+
+
+
+// tests 
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
